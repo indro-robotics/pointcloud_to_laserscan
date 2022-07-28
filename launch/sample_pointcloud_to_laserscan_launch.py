b6d12107b05207348ddcf7814dbe77cba6f56569
Austin Greisman <92941098+austin-InDro@users.noreply.github.com>
2022-07-28 11:14:39 -0400

    Updated to fit sentinel

--- launch/sample_pointcloud_to_laserscan_launch.py
+++ launch/sample_pointcloud_to_laserscan_launch.py
@@ -27,16 +27,16 @@ def generate_launch_description():
             remappings=[('cloud_in', ['/velodyne_points']),
                         ('scan', ['/scan'])],
             parameters=[{
-                'target_frame': 'velodyne',
+                'target_frame': 'base_link',
                 'transform_tolerance': 0.01,
-                'min_height': 0.0,
-                'max_height': 1.0,
-                'angle_min': -1.5708,  # -M_PI/2
-                'angle_max': 1.5708,  # M_PI/2
+                'min_height': 0.1,
+                'max_height': 1.2,
+                'angle_min': -3.14159,  # -M_PI/2
+                'angle_max': 3.14159,  # M_PI/2
                 'angle_increment': 0.0087,  # M_PI/360.0
                 'scan_time': 0.3333,
                 'range_min': 0.45,
-                'range_max': 4.0,
+                'range_max': 50.0,
                 'use_inf': True,
                 'inf_epsilon': 1.0
             }],
