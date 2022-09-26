2ccb98c373b6191e153f7ef40807e8d0ded4ce67
Austin Greisman <92941098+austin-InDro@users.noreply.github.com>
2022-09-26 15:06:01 -0400

    Update sample_pointcloud_to_laserscan_launch.py

--- launch/sample_pointcloud_to_laserscan_launch.py
+++ launch/sample_pointcloud_to_laserscan_launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
         ),
         Node(
             package='pointcloud_to_laserscan', executable='pointcloud_to_laserscan_node',
-            remappings=[('cloud_in', [LaunchConfiguration(variable_name='scanner'), '/cloud']),
+            remappings=[('cloud_in', ['/velodyne_points']),
                         ('scan', ['/scan'])],
             parameters=[{
                 'target_frame': 'base_link',
